f05fc9191f5e189c774751907398205732d7f65b
Shangyu Jiang <70888829+shangyuj@users.noreply.github.com>
2021-11-29 09:44:57 -0600

    Update project3.py

--- project3.py
+++ project3.py
@@ -6,23 +6,28 @@ textfile = open(filename)
 text = textfile.read()
 textfile.close()
 
-text = re.sub(r"(\w{2,})([cdglmnrstv])ise", "\\1\\2ize", text, 0, re.MULTILINE | re.IGNORECASE)
-text = re.sub(r"(\w{2,})([cdglmnrstv])ising", "\\1\\2izing", text, 0, re.MULTILINE | re.IGNORECASE)
-
-text = re.sub(r"(\w{2,})our", "\\1or", text, 0, re.MULTILINE | re.IGNORECASE)
-
-text = re.sub(r"(\w{2,})isation", "\\1ization", text, 0, re.MULTILINE | re.IGNORECASE)
-
+# Convert -gramme to -gram
 text = re.sub(r"gramme", "gram", text, 0, re.MULTILINE | re.IGNORECASE)
 
+# Convert -our to -or, avoid matching words like "tour"
+text = re.sub(r"(\w{2,})our", "\\1or", text, 0, re.MULTILINE | re.IGNORECASE)
+
+# Convert -logue to -log
 text = re.sub(r"(\w{2,})logue\b", "\\1log", text, 0, re.MULTILINE | re.IGNORECASE)
 text = re.sub(r"(\w{2,})logued\b", "\\1loged", text, 0, re.MULTILINE | re.IGNORECASE)
 text = re.sub(r"(\w{2,})logues\b", "\\1logs", text, 0, re.MULTILINE | re.IGNORECASE)
 text = re.sub(r"(\w{2,})loguing\b", "\\1loging", text, 0, re.MULTILINE | re.IGNORECASE)
+
+# Convert -ise to -ize, proceeded by the letter c, d, g, l, m, n, r, s, t, or v, avoid words like "rise"
+text = re.sub(r"(\w{2,})([cdglmnrstv])ise", "\\1\\2ize", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"(\w{2,})([cdglmnrstv])ising", "\\1\\2izing", text, 0, re.MULTILINE | re.IGNORECASE)
+
+# Convert -isation to -ization
+text = re.sub(r"(\w{2,})isation", "\\1ization", text, 0, re.MULTILINE | re.IGNORECASE)
 
 textfile_out = open(f'{filename[:-4]}_Americanized.txt', "w")
 for line in text:
     textfile_out.write(line)
 textfile_out.close()
 
-print(f'The output is saved in {filename[:-4]}_Americanized.txt')
+print(f'The output is saved in "{filename[:-4]}_Americanized.txt"')
