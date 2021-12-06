9c841d0cfcb9e80a18ccdc90caa9b54f2208a8ba
Shangyu Jiang <70888829+shangyuj@users.noreply.github.com>
2021-12-06 10:15:34 -0600

    Update project3.py

--- project3.py
+++ project3.py
@@ -2,30 +2,33 @@
 
 import re
 
+# Let users specify the filename and assign it to the variable "filename"
 filename = input("Please type in the filename of the text (extension included): ")
 
+# Open the text file, read the content, assign the content to the variable "text", and close the file
 textfile = open(filename)
 text = textfile.read()
 textfile.close()
 
 # Convert -gramme to -gram
-text = re.sub(r"gramme", "gram", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"gramme", r"gram", text, 0, re.MULTILINE | re.IGNORECASE)
 
-# Convert -our to -or, avoid matching words like "tour"
+# Convert -our to -or, avoid matching -our that is proceeded by just one letter, like "tour"
 text = re.sub(r"(\w{2,})our", r"\1or", text, 0, re.MULTILINE | re.IGNORECASE)
 
-# Convert -logue to -log
-text = re.sub(r"(\w{2,})logue\b", r"\1log", text, 0, re.MULTILINE | re.IGNORECASE)
-text = re.sub(r"(\w{2,})logued\b", r"\1loged", text, 0, re.MULTILINE | re.IGNORECASE)
-text = re.sub(r"(\w{2,})logues\b", r"\1logs", text, 0, re.MULTILINE | re.IGNORECASE)
-text = re.sub(r"(\w{2,})loguing\b", r"\1loging", text, 0, re.MULTILINE | re.IGNORECASE)
+# Convert -logue to -log, avoid matching the standalone word "logue"
+text = re.sub(r"\Blogue\b", r"log", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"\Blogued\b", r"logged", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"\Blogues\b", r"logs", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"\Bloguing\b", r"logging", text, 0, re.MULTILINE | re.IGNORECASE)
 
-# Convert -ise to -ize, proceeded by the letter c, d, g, l, m, n, r, s, t, or v, avoid words like "rise"
+# Convert -ise to -ize, proceeded by the letter c, d, g, l, m, n, r, s, t, or v
+# Avoid matching -ise that is proceeded by just one letter, like "rise"
 text = re.sub(r"(\w{2,})([cdglmnrstv])ise", r"\1\2ize", text, 0, re.MULTILINE | re.IGNORECASE)
 text = re.sub(r"(\w{2,})([cdglmnrstv])ising", r"\1\2izing", text, 0, re.MULTILINE | re.IGNORECASE)
 
 # Convert -isation to -ization
-text = re.sub(r"(\w{2,})isation", r"\1ization", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"isation", r"ization", text, 0, re.MULTILINE | re.IGNORECASE)
 
 textfile_out = open(f'{filename[:-4]}_Americanized.txt', "w")
 for line in text:
