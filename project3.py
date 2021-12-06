908ba16934d685895b0fd669c62136ab08a3a26b
Shangyu Jiang <70888829+shangyuj@users.noreply.github.com>
2021-12-06 09:38:13 -0600

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,3 +1,5 @@
+# This program converts British spellings to American spellings in a text file.
+
 import re
 
 filename = input("Please type in the filename of the text (extension included): ")
@@ -10,7 +12,7 @@ textfile.close()
 text = re.sub(r"gramme", "gram", text, 0, re.MULTILINE | re.IGNORECASE)
 
 # Convert -our to -or, avoid matching words like "tour"
-text = re.sub(r"(\w{2,})our", "\\1or", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"(\w{2,})our", r"\1or", text, 0, re.MULTILINE | re.IGNORECASE)
 
 # Convert -logue to -log
 text = re.sub(r"(\w{2,})logue\b", r"\1log", text, 0, re.MULTILINE | re.IGNORECASE)
@@ -19,11 +21,11 @@ text = re.sub(r"(\w{2,})logues\b", r"\1logs", text, 0, re.MULTILINE | re.IGNOREC
 text = re.sub(r"(\w{2,})loguing\b", r"\1loging", text, 0, re.MULTILINE | re.IGNORECASE)
 
 # Convert -ise to -ize, proceeded by the letter c, d, g, l, m, n, r, s, t, or v, avoid words like "rise"
-text = re.sub(r"(\w{2,})([cdglmnrstv])ise", "\\1\\2ize", text, 0, re.MULTILINE | re.IGNORECASE)
-text = re.sub(r"(\w{2,})([cdglmnrstv])ising", "\\1\\2izing", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"(\w{2,})([cdglmnrstv])ise", r"\1\2ize", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"(\w{2,})([cdglmnrstv])ising", r"\1\2izing", text, 0, re.MULTILINE | re.IGNORECASE)
 
 # Convert -isation to -ization
-text = re.sub(r"(\w{2,})isation", "\\1ization", text, 0, re.MULTILINE | re.IGNORECASE)
+text = re.sub(r"(\w{2,})isation", r"\1ization", text, 0, re.MULTILINE | re.IGNORECASE)
 
 textfile_out = open(f'{filename[:-4]}_Americanized.txt', "w")
 for line in text:
